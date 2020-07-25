30a83fd593ca77698a9f92718ba3dd63a76554dd
usulthan <67325670+usulthan@users.noreply.github.com>
2020-07-25 08:38:50 +0530

    This is one of the game Application

---
+++ IHaveToFly/app/src/main/java/com/example/ihavetofly/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.ihavetofly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean isMute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,GameActivity.class));
+            }
+        });
+
+        TextView highScoreText= findViewById(R.id.highScoreTxt);
+        final SharedPreferences prefs = getSharedPreferences("game",MODE_PRIVATE);
+        highScoreText.setText("HighScore: "+prefs.getInt("highScore",0));
+        isMute=prefs.getBoolean("isMute",false);
+
+        final ImageView volumeCtrl= findViewById(R.id.volumeCtrl);
+        if (isMute)
+            volumeCtrl.setImageResource(R.drawable.ic_volume_off_black_24dp);
+        else
+            volumeCtrl.setImageResource(R.drawable.ic_volume_up_black_24dp);
+            volumeCtrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isMute = !isMute;
+                if (isMute)
+                    volumeCtrl.setImageResource(R.drawable.ic_volume_off_black_24dp);
+                else
+                    volumeCtrl.setImageResource(R.drawable.ic_volume_up_black_24dp);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("isMute",isMute);
+                editor.apply();
+            }
+        });
+
+
+    }
+}
